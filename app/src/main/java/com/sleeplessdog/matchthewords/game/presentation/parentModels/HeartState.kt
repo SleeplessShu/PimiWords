c47f640cb91624438665c23c3d3c9d55fb391d7c
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2025-12-30 21:57:28 +0300

    Делаю анимации

--- app/src/main/java/com/sleeplessdog/matchthewords/game/presentation/parentModels/HeartState.kt
+++ app/src/main/java/com/sleeplessdog/matchthewords/game/presentation/parentModels/HeartState.kt
@@ -1,3 +1,8 @@
 package com.sleeplessdog.matchthewords.game.presentation.parentModels
 
-enum class HeartState { FULL, HALF, LOW, HIDDEN }
+enum class HeartState {
+    FAST_DARK_TO_MEDIUM,    // 1 → 2: темнооранжевый быстро меняется на среднеоранжевый
+    APPEAR_MEDIUM_ORANGE,   // Появление среднего оранжевого сердца (get_heart_to_2)
+    MEDIUM_TO_BRIGHT,       // 2 → 3: среднеоранжевый плавно становится яркооранжевым
+    APPEAR_BRIGHT_ORANGE    // Появление яркого оранжевого сердца (get_heart_to_3)
+}
