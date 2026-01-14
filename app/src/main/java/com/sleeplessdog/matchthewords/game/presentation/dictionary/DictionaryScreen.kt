dfe2cdef7327f147ff6667294306b70f24f4df4f
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2026-01-14 14:07:30 +0300

    Делаю вертску Словаря

--- app/src/main/java/com/sleeplessdog/matchthewords/game/presentation/dictionary/DictionaryScreen.kt
+++ app/src/main/java/com/sleeplessdog/matchthewords/game/presentation/dictionary/DictionaryScreen.kt
@@ -1,5 +1,6 @@
 package com.sleeplessdog.matchthewords.game.presentation.dictionary
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -15,20 +16,27 @@ import androidx.compose.material3.Icon
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Divider
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sleeplessdog.matchthewords.R
+import com.sleeplessdog.matchthewords.ui.theme.BlackPrimary
+import com.sleeplessdog.matchthewords.ui.theme.DarkTextDefault
+import com.sleeplessdog.matchthewords.ui.theme.Gray03
+import com.sleeplessdog.matchthewords.ui.theme.White01
+import com.sleeplessdog.matchthewords.ui.theme.textSize16Bold
+import com.sleeplessdog.matchthewords.ui.theme.textSize20Medium
+import com.sleeplessdog.matchthewords.ui.theme.textSize24Medium
 
 @Composable
 fun DictionaryScreen(listWord: List<String>, listStandardWord: List<String>) {
-    Column(modifier = Modifier.background()) {
+    Column(modifier = Modifier.background(BlackPrimary)) {
         HeaderDictionary()
         Spacer(modifier = Modifier.height(8.dp))
         MyGroupsHeader()
@@ -48,28 +56,35 @@ fun HeaderDictionary() {
         modifier = Modifier
             .fillMaxWidth()
             .height(48.dp)
-            .background(Color.LightGray) // пример фона заголовка
     ) {
         Row(
             modifier = Modifier.fillMaxSize(),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_app_name),
+            Image(
+                painter = painterResource(id = R.drawable.pimiss),
                 contentDescription = "Левая иконка",
-                modifier = Modifier.padding(start = 16.dp)
+                modifier = Modifier
+                    .padding(start = 16.dp)
             )
             Spacer(modifier = Modifier.weight(1f))
             Text(
-                text = "Словарь",
-                style = MaterialTheme.typography.bodySmall,
+                text = stringResource(R.string.dictionary),
+                style = textSize24Medium,
+                color = DarkTextDefault,
                 modifier = Modifier.align(Alignment.CenterVertically)
             )
             Spacer(modifier = Modifier.weight(1f))
             Icon(
-                painter = painterResource(id = R.drawable.ic_app_name),
+                painter = painterResource(id = R.drawable.icon_park_outline_search),
                 contentDescription = "Правая иконка",
-                modifier = Modifier.padding(end = 16.dp)
+                tint = DarkTextDefault,
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .clickable {
+                        // Действие при клике
+                        println("Иконка нажата!")
+                    }
             )
         }
     }
@@ -83,15 +98,24 @@ fun MyGroupsHeader() {
             .padding(start = 18.dp, end = 20.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = "Мои группы")
+        Text(
+            text = "Мои группы",
+            style = textSize20Medium,
+            color = DarkTextDefault
+        )
         Spacer(modifier = Modifier.weight(1f))
         Row(verticalAlignment = Alignment.CenterVertically) {
             Icon(
-                painter = painterResource(id = R.drawable.b_tof_no),
+                painter = painterResource(id = R.drawable.icon_visibility),
+                tint = DarkTextDefault,
                 contentDescription = "Иконка все"
             )
             Spacer(modifier = Modifier.width(6.dp))
-            Text(text = "Все")
+            Text(
+                style = textSize16Bold,
+                color = Gray03,
+                text = "Все",
+            )
         }
     }
 }
@@ -128,29 +152,37 @@ fun MyGroupsTable(listWord: List<String>) {
 
 @Composable
 fun MyGroupTableRow(rowIndex: Int, stringWord: String) {
-    val iconPainter = painterResource(id = R.drawable.ic_app_name) // замени ресурс
-    val clickableIconPainter = painterResource(id = R.drawable.ic_app_name) // замени ресурс
-
+    val clickableIconPainter =
+        painterResource(id = R.drawable.icon_dots_three_outline_vertical) // замени ресурс
+    val leftIconPainter = when (rowIndex) {
+        -1 -> painterResource(R.drawable.icon_favorite)    // первая строка
+        -2 -> painterResource(R.drawable.icon_add)    // последняя строка
+        else -> painterResource(R.drawable.icon_book)       // обычные строки
+    }
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .height(56.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        // Левая иконка — всегда
         Icon(
-            painter = iconPainter,
+            painter = leftIconPainter,
+            tint = DarkTextDefault,
             contentDescription = "Icon for row $rowIndex",
             modifier = Modifier.size(24.dp)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Text(stringWord)
+        Text(
+            stringWord,
+            style = textSize16Bold,
+            color = DarkTextDefault
+        )
         Spacer(modifier = Modifier.weight(1f))
 
-        // Правая иконка рисуется только если строка не первая и не последняя
         if (rowIndex != -1 && rowIndex != -2) {
             Icon(
                 painter = clickableIconPainter,
+                tint = DarkTextDefault,
                 contentDescription = "Clickable icon for row $rowIndex",
                 modifier = Modifier
                     .padding(end = 10.dp)
@@ -171,15 +203,24 @@ fun StandardGroupsHeader() {
             .padding(start = 18.dp, end = 20.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(text = "Стандартные группы")
+        Text(
+            text = "Стандартные группы",
+            style = textSize20Medium,
+            color = DarkTextDefault
+        )
         Spacer(modifier = Modifier.weight(1f))
         Row(verticalAlignment = Alignment.CenterVertically) {
             Icon(
-                painter = painterResource(id = R.drawable.b_tof_no),
+                painter = painterResource(id = R.drawable.icon_visibility),
+                tint = DarkTextDefault,
                 contentDescription = "Иконка все"
             )
             Spacer(modifier = Modifier.width(6.dp))
-            Text(text = "Все")
+            Text(
+                text = "Все",
+                style = textSize16Bold,
+                color = Gray03,
+            )
         }
     }
 }
@@ -209,12 +250,15 @@ fun StandardGroupTableRow(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
-            painter = painterResource(id = R.drawable.ic_app_name),
+            painter = painterResource(id = R.drawable.icon_add_standard_group),
             contentDescription = null,
+            tint = DarkTextDefault,
             modifier = Modifier.size(24.dp)
         )
         Spacer(modifier = Modifier.width(16.dp))
         Text(
+            style = textSize16Bold,
+            color = DarkTextDefault,
             text = text,
             modifier = Modifier.weight(1f)
         )
@@ -223,7 +267,8 @@ fun StandardGroupTableRow(
             println("Кнопка для $text нажата")
         }) {
             Icon(
-                painter = painterResource(id = R.drawable.ic_app_name),
+                painter = painterResource(id = R.drawable.icon_play_circle),
+                tint = White01,
                 contentDescription = "Кнопка действия"
             )
         }
---
+++ app/src/main/java/com/sleeplessdog/matchthewords/ui/theme/Typography.kt
@@ -0,0 +1,40 @@
+package com.sleeplessdog.matchthewords.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.sleeplessdog.matchthewords.R
+
+val myFontFamily = FontFamily(
+    Font(R.font.a_bee_zee_regular, FontWeight.W400, FontStyle.Normal),
+    Font(R.font.open_sans_bold, FontWeight.W700, FontStyle.Normal),
+    Font(R.font.open_sans_bold, FontWeight.W600, FontStyle.Normal),
+)
+val textSize24Medium = TextStyle(
+    fontStyle = FontStyle.Normal,
+    fontWeight = FontWeight.Normal,
+    fontFamily = myFontFamily,
+    letterSpacing = 0.sp,
+    lineHeight = 24.sp,
+    fontSize = 24.sp,
+)
+
+val textSize20Medium = TextStyle(
+    fontStyle = FontStyle.Normal,
+    fontWeight = FontWeight.Normal,
+    fontFamily = myFontFamily,
+    letterSpacing = 0.sp,
+    lineHeight = 20.sp,
+    fontSize = 20.sp,
+)
+val textSize16Bold = TextStyle(
+    fontStyle = FontStyle.Normal,
+    fontWeight = FontWeight.Bold,
+    fontFamily = myFontFamily,
+    letterSpacing = 0.sp,
+    lineHeight = 21.sp,
+    fontSize = 20.sp,
+)
